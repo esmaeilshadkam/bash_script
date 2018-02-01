3033230d04592331cbe8b33dae7f248c17899877
esmaeilshadkam <34976303+esmaeilshadkam@users.noreply.github.com>
2018-02-02 02:46:11 +0330

    Update ddos-test.sh

--- ddos-test.sh
+++ ddos-test.sh
@@ -3,10 +3,11 @@ netstat -tn 2>/dev/null | grep :443 | awk '{print $5}' | cut -d: -f1 | sort | un
 conn_1=`cat /tmp/ddos-test |head -n 1 | awk '{print $1}'`
 ip_1=`cat /tmp/ddos-test |head -n 1 | awk '{print $2}'`
 ##
-if [ $conn_1 -gt 40 ]; then
+if [ $conn_1 -gt 50 ]; then
 ##
 TIME_DATE=`date +%Y-%m-%d_%H.%M.%S`
 ##
+netstat -tn 2>/dev/null | grep :443 | awk '{print $5}' | cut -d: -f1 | sort | uniq -c | sort -nr | head > /tmp/ddos-log.$RANDOM
 ip_2=`cat /tmp/ddos-test |head -n 2 | awk '{print $2}' | tail -n1`
 ip_3=`cat /tmp/ddos-test |head -n 3 | awk '{print $2}' | tail -n1`
 ip_4=`cat /tmp/ddos-test |head -n 4 | awk '{print $2}' | tail -n1`
@@ -17,16 +18,16 @@ ip_8=`cat /tmp/ddos-test |head -n 8 | awk '{print $2}' | tail -n1`
 ip_9=`cat /tmp/ddos-test |head -n 9 | awk '{print $2}' | tail -n1`
 ip_10=`cat /tmp/ddos-test |head -n 10 | awk '{print $2}' | tail -n1`
 ##
-whois_ip_1=`/usr/bin/whois $ip_1 | grep -iE ^country: | tail -1`
-whois_ip_2=`/usr/bin/whois $ip_2 | grep -iE ^country: | tail -1`
-whois_ip_3=`/usr/bin/whois $ip_3 | grep -iE ^country: | tail -1`
-whois_ip_4=`/usr/bin/whois $ip_4 | grep -iE ^country: | tail -1`
-whois_ip_5=`/usr/bin/whois $ip_5 | grep -iE ^country: | tail -1`
-whois_ip_6=`/usr/bin/whois $ip_6 | grep -iE ^country: | tail -1`
-whois_ip_7=`/usr/bin/whois $ip_7 | grep -iE ^country: | tail -1`
-whois_ip_8=`/usr/bin/whois $ip_8 | grep -iE ^country: | tail -1`
-whois_ip_9=`/usr/bin/whois $ip_9 | grep -iE ^country: | tail -1`
-whois_ip_10=`/usr/bin/whois $ip_10 | grep -iE ^country: | tail -1`
+whois_ip_1=`/usr/bin/curl ipinfo.io/$ip_1/country 2> /dev/null`
+whois_ip_2=`/usr/bin/curl ipinfo.io/$ip_2/country 2> /dev/null`
+whois_ip_3=`/usr/bin/curl ipinfo.io/$ip_3/country 2> /dev/null`
+whois_ip_4=`/usr/bin/curl ipinfo.io/$ip_4/country 2> /dev/null`
+whois_ip_5=`/usr/bin/curl ipinfo.io/$ip_5/country 2> /dev/null`
+whois_ip_6=`/usr/bin/curl ipinfo.io/$ip_6/country 2> /dev/null`
+whois_ip_7=`/usr/bin/curl ipinfo.io/$ip_7/country 2> /dev/null`
+whois_ip_8=`/usr/bin/curl ipinfo.io/$ip_8/country 2> /dev/null`
+whois_ip_9=`/usr/bin/curl ipinfo.io/$ip_9/country 2> /dev/null`
+whois_ip_10=`/usr/bin/curl ipinfo.io/$ip_10/country 2> /dev/null`
 ##
 echo "DDOS attack from $ip_1 -> $whois_ip_1 -> $TIME_DATE" > /tmp/ddos-alert
 echo "DDOS attack from $ip_2 -> $whois_ip_2 -> $TIME_DATE" >> /tmp/ddos-alert
@@ -38,7 +39,10 @@ echo "DDOS attack from $ip_7 -> $whois_ip_7 -> $TIME_DATE" >> /tmp/ddos-alert
 echo "DDOS attack from $ip_8 -> $whois_ip_8 -> $TIME_DATE" >> /tmp/ddos-alert
 echo "DDOS attack from $ip_9 -> $whois_ip_9 -> $TIME_DATE" >> /tmp/ddos-alert
 echo "DDOS attack from $ip_10 -> $whois_ip_10 -> $TIME_DATE" >> /tmp/ddos-alert
-##
+#
+#ssmtp
+cat /tmp/ddos-alert  | mail  -a from:site.com@gmail.com -s "DDOS Alert! site.com" -- esmaeil[dot]shadkam[at]gmail[dot]com
+/usr/local/bin/telegram-notify --text "ddos-alert" --document "/tmp/ddos-alert" > /dev/null
 else
   echo "no find ddos attack !" > /dev/null
 fi;
